346c4993c476e79a83e9ca986cfd582aae9bec09
ewhite1999 <ewhite1999@gmail.com>
2022-03-30 09:52:31 +0100

    add db model

--- backend/app.py
+++ backend/app.py
@@ -11,6 +11,11 @@ if __name__ == "__main__":
     server.run(debug=True)
 
 
+class URLS(db.Model):
+    long_url = db.Column(db.String(200), unique=True, nullable=False)
+    short_url = db.Column(db.String(200), unique=True, nullable=False)
+
+
 @app.route('/')
 def home():
     return 'Hello, World!'
