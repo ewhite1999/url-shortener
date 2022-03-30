fa3c532bd6539443f54c2aaea5408729a25eb79c
ewhite1999 <ewhite1999@gmail.com>
2022-03-30 14:53:49 +0100

    add test routes for post url

--- backend/test_app.py
+++ backend/test_app.py
@@ -2,11 +2,21 @@ import json
 
 
 class TestRoutes():
+
+    def test_get_urls(self, api):
+        res = api.get('/urls')
+        assert b"short_url" in res.data
+        assert b"long_url" in res.data
+
     def test_post_urls(self, api):
         mock_headers = {'Content-Type': 'application/json'}
         fakeData = json.dumps({
             "long_url": "https://pytest-flask.readthedocs.io/en/latest/tutorial.html"
         })
-        respons = api.post('/urls', data=fakeData, headers=mock_headers)
-        assert respons.status == '201 ADDED'
-        assert len(respons.data) == 5
+        res = api.post('/urls', data=fakeData, headers=mock_headers)
+        assert res.status == '200 OK'
+
+    def test_home_route(self, api):
+        res = api.get('/')
+        assert res.data == b'Hello, World!'
+        assert res.status == '200 OK'
