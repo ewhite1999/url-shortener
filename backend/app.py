59bac5804291a51b03ee660856768815a7cee27e
ewhite1999 <ewhite1999@gmail.com>
2022-03-30 15:27:26 +0100

    allow cors

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,12 @@
 from http import server
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import exc
 import helpers
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urls.db'
 db = SQLAlchemy(app)
 
