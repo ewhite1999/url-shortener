7861d67f3da7e2088ed09abb833aa17eb9101345
ewhite1999 <ewhite1999@gmail.com>
2022-03-30 12:07:06 +0100

    remove / from short urls

--- backend/helpers.py
+++ backend/helpers.py
@@ -2,9 +2,8 @@ import random
 
 
 def shorten_url(long_url):
-    # TODO: Add some checking to make sure it hasn't randomly added the same url
-    # TODO: rm special chars
-    return ''.join(random.choice(long_url) for _ in range(5))
+    clean_url = long_url.replace('/', '')
+    return ''.join(random.choice(clean_url) for _ in range(5))
 
 
 def find_long_url(short_url, db):
