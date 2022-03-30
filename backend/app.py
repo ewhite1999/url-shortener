2c8e555db925aa87209a1f4a78e3a640d6cdd2c3
ewhite1999 <ewhite1999@gmail.com>
2022-03-30 10:21:45 +0100

    slight refactor

--- backend/app.py
+++ backend/app.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
 
 class URLS(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    long_url = db.Column(db.String(10), unique=True, nullable=False)
-    short_url = db.Column(db.String(200), unique=True, nullable=False)
+    long_url = db.Column(db.String(200), unique=True, nullable=False)
+    short_url = db.Column(db.String(10), unique=True, nullable=False)
 
 
 @app.route('/')
@@ -30,16 +30,16 @@ def urls_routes():
         urlsList = [{'long_url': url.long_url, 'short_url': url.short_url}
                     for url in urls]
         return jsonify(urlsList), 200
+
     if request.method == 'POST':
         newData = request.json
         long_url = newData['long_url']
         # TODO: make this function
         short_url = shorten_url(long_url)
-        # TODO: need to check if URL already in db first
+        # TODO: need to check if URL already in db first (try except block)
         newUrl = URLS(long_url=long_url, short_url=short_url)
         db.session.add(newUrl)
         db.session.commit()
-        urls = URLS.query.all()
-        urlsList = [{'long_url': url.long_url, 'short_url': url.short_url}
-                    for url in urls]
+        urlsList = {'long_url': long_url, 'short_url': short_url}
+
         return jsonify(urlsList), 201
