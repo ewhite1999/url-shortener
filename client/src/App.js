ea6ee83fea367fd909ffa5451bd3cebfe4e2fb1f
capriceebanks <capri.ee@outlook.com>
2022-03-30 12:39:13 +0100

    added clientside

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import { Switch, Route } from 'react-router-dom';
+
+
+import { Search } from './components'
+
+
+const App = () => {
+return(
+<>
+<Search />
+{/* <Switch>
+<Route exact path='/'><Home /></Route> */}
+{/* <Route path='/'>< /></Route> */}
+{/* </Switch> */}
+</>
+)};
+
+export default App;
---
+++ client/src/components/Search/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Search = () => {
+return(
+<>
+<h1>URL Shortener</h1>
+
+</>
+)};
+export default Search;
