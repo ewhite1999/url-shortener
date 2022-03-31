959280d2023903450871d0c83628e2f971cbd827
ewhite1999 <ewhite1999@gmail.com>
2022-03-31 08:37:13 +0100

    basic styling

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -28,17 +28,40 @@ const Search = () => {
   };
   return (
     <>
-      <h1 className="border-black border-4">URL Shortener</h1>
-      <h3>Shorten your URL for FREE by pasting link in box below</h3>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="url">Enter Link</label>
-        <input type="input" id="url" name="url" placeholder="Paste Link Here" />
-        <input type="submit" />
+      <h1 className="text-4xl text-center pb-4 mt-8">URL Shortener</h1>
+      <h3 className="text-2xl text-center pb-4">
+        Shorten your URL for FREE by pasting link in box below
+      </h3>
+      <form onSubmit={handleSubmit} className="flex flex-col max-w-xl mx-auto">
+        <div className="flex space-x-2">
+          <label htmlFor="url" className="p-2">
+            Enter Link:
+          </label>
+          <input
+            type="input"
+            id="url"
+            name="url"
+            placeholder="Paste Link Here"
+            className="border-b-2 w-96 p-2"
+          />
+        </div>
+        <input
+          type="submit"
+          className="mt-8 py-2 px-4 bg-slate-400 text-slate-900 mx-auto"
+        />
       </form>
-      <div>
+      <div className="flex flex-col max-w-xl mx-auto space-y-2 mt-4">
         {message && <p>We already processed that url, here you go</p>}
-        {longURL && <a href={longURL}>Long URL: {longURL}</a>}
-        {shortURL && <a href={shortURL}>Short URL: {shortURL}</a>}
+        {longURL && (
+          <a className=" text-blue-700 hover:text-blue-300" href={longURL}>
+            Long URL: {longURL}
+          </a>
+        )}
+        {shortURL && (
+          <a className=" text-blue-700 hover:text-blue-300" href={shortURL}>
+            Short URL: {shortURL}
+          </a>
+        )}
       </div>
     </>
   );
