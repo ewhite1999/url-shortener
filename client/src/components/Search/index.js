a1af7f3f698c4aeafdbdc544994dd970aabeb5ce
ewhite1999 <ewhite1999@gmail.com>
2022-03-31 07:43:12 +0100

    fix for on form

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 const Search = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
   return (
     <>
       <h1 className="border-black border-4">URL Shortener</h1>
       <h3>Shorten your URL for FREE by pasting link in box below</h3>
-      <form>
-        <label for="url">Enter Link</label>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="url">Enter Link</label>
         <input type="input" id="url" name="url" placeholder="Paste Link Here" />
         <input type="submit" />
       </form>
